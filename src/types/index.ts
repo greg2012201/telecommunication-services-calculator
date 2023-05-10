20e5cc8dffc75f9ceacbdcdf5c7fde13f627de10
Grzegorz Dubiel <greg2012201@gmail.com>
2023-05-10 17:39:50 +0200

    Imporve types and add type guard.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,20 @@
 type ProductKey = string;
 
+export function isPackage(x: any): x is Package {
+  return x.type === "PACKAGE";
+}
 export interface Product {
   name: string;
+  description: string;
   productKey: ProductKey;
+  type: "ITEM" | "PACKAGE";
   price: {
     2023: number;
     2024: number;
     2025: number;
   };
-  includedProduct: ProductKey[];
+}
+
+export interface Package extends Product {
+  includedProducts: ProductKey[];
 }
