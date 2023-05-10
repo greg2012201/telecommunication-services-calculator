cf6d678dfc9e837c3f61b354da6a668ea45d048a
Grzegorz Dubiel <greg2012201@gmail.com>
2023-05-10 16:52:51 +0200

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="pt-8 text-2xl">Telco Services calculator</h1>
+      <p>Find your best offer</p>
+    </div>
+  );
+}
+
+export default App;
