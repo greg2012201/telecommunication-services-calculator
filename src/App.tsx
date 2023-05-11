e5a74543bc012380772c82e685d7aef15b9fd3bc
Grzegorz Dubiel <greg2012201@gmail.com>
2023-05-12 00:44:18 +0200

    Switch from tailwind to css modules.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
+import ProductList from "./components/ProductList";
 import { ProductContainer, ProductProvider } from "./services/Product";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <ProductProvider>
-      <div className="flex flex-col items-center">
-        <h1 className="pt-8 text-2xl">Telco Services calculator</h1>
-        <p>Find your best offer</p>
+      <div className={styles.layout}>
+        <div className={styles.title_wrapper}>
+          <h1 className={styles.title}>Telco Services calculator</h1>
+          <p>Find your best offer</p>
+        </div>
+        <ProductContainer>
+          <ProductList />
+        </ProductContainer>
       </div>
-      <ProductContainer>
-        <p>hello from container</p>
-      </ProductContainer>
     </ProductProvider>
   );
 }
