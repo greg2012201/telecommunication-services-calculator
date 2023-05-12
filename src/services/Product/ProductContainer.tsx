746c07c58cc9b14330d5ac59760c2e483a8ebd9a
Grzegorz Dubiel <greg2012201@gmail.com>
2023-05-13 00:58:49 +0200

    Fix children prop.

--- src/services/Product/ProductContainer.tsx
+++ src/services/Product/ProductContainer.tsx
@@ -1,14 +1,14 @@
-import { FC } from "react";
 import { fetchManager } from "../API";
 import useFetch from "../../hooks/useFetch";
 import { TProduct } from "../../types";
 import { useProduct } from ".";
+import { ReactNode } from "react";
 
 interface Props {
-  children: JSX.Element;
+  children: ReactNode;
 }
 
-const ProductContainer: FC<Props> = ({ children }) => {
+function ProductContainer({ children }: Props) {
   const { dispatch } = useProduct();
   const fetcher = fetchManager(import.meta.env.VITE_REACT_APP_FEATURE_FLAG);
   const { isFetching } = useFetch<TProduct[]>({
@@ -21,7 +21,7 @@ const ProductContainer: FC<Props> = ({ children }) => {
   if (isFetching) {
     return <p>Loading...</p>;
   }
-  return children;
-};
+  return <>{children}</>;
+}
 
 export default ProductContainer;
